436b05b305a49c21a7536308549e986580a31443
kvi <kvi92@ya.ru>
2023-12-31 19:51:49 +0300

    Тестовые сниппеты для Api Search
    Тестовый вариант сниппетов, в котором:
    - не тестированы частные случаи поиска;
    - сворачивание лишних результатов (более 3 строк) сделано стандартным тегом details.

--- src/main/java/searchengine/services/LemmasFinder.java
+++ src/main/java/searchengine/services/LemmasFinder.java
@@ -1,5 +1,6 @@
 package searchengine.services;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.lucene.morphology.LuceneMorphology;
 import org.apache.lucene.morphology.english.EnglishLuceneMorphology;
@@ -7,9 +8,7 @@ import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
 import org.jsoup.Jsoup;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
@@ -28,34 +27,24 @@ public class LemmasFinder {
      * @param text текст, в котором будут найдены русские и английские леммы
      * @return соответствие, где ключ - лемма, а значение - сколько раз она встречается в text
      */
-    public HashMap<String, Integer> findLemmas(String text) {
+    public HashMap<String, Integer> findLemmas(@NonNull String text) {
         var lemmas = new HashMap<String, Integer>();
 
         var words = getWords(text);
         for (var word : words) {
-            LuceneMorphology morphology;
-
-            if (russianMorphology.checkString(word)) {
-                morphology = russianMorphology;
-            } else {
-                morphology = englishMorphology;
+            var normalWord = getNormalWord(word);
+            if (normalWord.isBlank()) {
+                continue;
             }
 
-            // Некоторые слова могут иметь несколько нормальных форм.
-            // Например, у test, tested одна форма - test, а у testing их две - testing и test (именно в таком порядке).
-            // Для поискового запроса достаточно одного варианта. Выбирается первый найденный - самый приоритетный.
-            var normalForms = morphology.getNormalForms(word);
-//            for (var normalWord : normalForms) {
-            var normalWord = normalForms.get(0);
             var count = lemmas.getOrDefault(normalWord, 0) + 1;
             lemmas.put(normalWord, count);
-//            }
         }
 
         return lemmas;
     }
 
-    public String htmlToText(String html) {
+    public String htmlToText(@NonNull String html) {
         return Jsoup.parse(html).text();
     }
 
@@ -66,9 +55,100 @@ public class LemmasFinder {
      * @param html текст html, в котором будут найдены русские и английские леммы
      * @return соответствие, где ключ - лемма, а значение - сколько раз она встречается в text
      */
-    public HashMap<String, Integer> findLemmasInHtml(String html) {
+    public HashMap<String, Integer> findLemmasInHtml(@NonNull String html) {
         var text = htmlToText(html);
         return findLemmas(text);
+    }
+
+
+    /**
+     * Выполняет поиск лемм, формирует сниппет результата поиска. Текст должен быть заранее очищено от html-тегов.
+     * @param lemmas искомые леммы
+     * @param text текст, очищенный от html-тегов
+     * @return сниппет
+     */
+    public String getSnippet(@NonNull String text, @NonNull Set<String> lemmas) {
+        var textLowerCase = text.toLowerCase(); // Для поиска без учёта регистра
+        var words = getWords(textLowerCase);
+
+        var snippet = new StringBuilder();
+        var rangeSize = 2; // Количество значимых слов слева и справа от леммы
+        var lastIndex = 0; // Последний проверенный символ текста
+        var lastLemmaIndex = -1;
+        var lastWordIndex = -1;
+        var spoilerAdded = false; // Если слишком много совпадений
+
+        for (var i = 0; i < words.size(); i++) {
+            var word = words.get(i);
+            var normalWord = getNormalWord(word);
+
+            if (lemmas.contains(normalWord)) {
+
+                if (i > 0 && lastLemmaIndex == -1) {
+                    snippet.append("... ");
+                }
+
+                if (snippet.length() > 270 && !spoilerAdded) {
+                    snippet.append("<details>");
+                    spoilerAdded = true;
+                }
+
+                var startWordIndex = textLowerCase.indexOf(word, lastIndex);
+
+                var previousIndex = Math.max(lastWordIndex, i - rangeSize);
+                if (previousIndex != -1 && previousIndex < i - 1) {
+                    var previousWord = words.get(previousIndex);
+                    var start = textLowerCase.lastIndexOf(previousWord, startWordIndex);
+                    var previousText = text.substring(start, startWordIndex);
+                    snippet.append(previousText);
+                }
+
+                snippet.append("<b>");
+                lastIndex = startWordIndex + word.length();
+                var wordText = text.substring(startWordIndex, lastIndex);
+                snippet.append(wordText);
+                snippet.append("</b>");
+
+                lastWordIndex = i;
+                lastLemmaIndex = i;
+
+                continue;
+            }
+
+            if (lastLemmaIndex == -1) {
+                continue;
+            }
+
+            var endWordIndex = lastLemmaIndex + rangeSize; // Текст "до" текущего слова
+            if (i <= endWordIndex) {
+                // Добавление подсказки после леммы
+                if (i == words.size() - 1) {
+                    var addingText = text.substring(lastIndex);
+                    snippet.append(addingText);
+                    break;
+                }
+
+                var nextWord = words.get(i + 1);
+                var end = textLowerCase.indexOf(nextWord, lastIndex);
+                var addingText = text.substring(lastIndex, end);
+                snippet.append(addingText);
+
+                lastIndex = end;
+                lastWordIndex = i;
+            } else if (i == (endWordIndex + 1)) {
+                var lastSymbol = snippet.substring(snippet.length() - 1);
+                if (!Objects.equals(lastSymbol, " ")) {
+                    snippet.append(" ");
+                }
+                snippet.append("... ");
+            }
+        }
+
+        if (spoilerAdded) {
+            snippet.append("</details>");
+        }
+
+        return snippet.toString().strip();
     }
 
     /**
@@ -77,7 +157,7 @@ public class LemmasFinder {
      * @param text анализируемый текст
      * @return слова
      */
-    private List<String> getWords(String text) {
+    private List<String> getWords(@NonNull String text) {
         var words = text.strip().toLowerCase().split("\\s+");
 
         return Arrays.stream(words)
@@ -110,26 +190,57 @@ public class LemmasFinder {
      * @param word проверяемое слово
      * @return слово удовлетворяет условиям
      */
-    private boolean isFittingWord(String word) {
+    private boolean isFittingWord(@NonNull String word) {
         if (word.isBlank()) {
             return false;
         }
 
-        if (russianMorphology.checkString(word)) {
-            return notParticle(word, russianMorphology);
-        } else if (englishMorphology.checkString(word)) {
-            return notParticle(word, englishMorphology);
+        var morphology = qualifyMorphology(word);
+        if (morphology == null) {
+            return false;
         }
 
-        return false;
-    }
-
-    private boolean notParticle(String word, LuceneMorphology morphology) {
         return morphology.getMorphInfo(word).stream()
                 .filter(s -> !s.isBlank())
                 .map(String::toUpperCase)
                 .map(s -> s.split("\\s+"))
                 .flatMap(Arrays::stream)
                 .noneMatch(PARTICLES::contains);
+    }
+
+    /**
+     * Возвращает нормальную форму слова для сохранения леммы в базу данных.
+     * Некоторые слова могут иметь несколько нормальных форм. Для поискового запроса достаточно одного варианта.
+     * В этом случае выбирается первый из списка нормальных форм - самый приоритетный.
+     * Например, у test и tested одна форма - test, а у testing их две - testing и test (именно в таком порядке).
+     *
+     * @param word обрабатываемое слово
+     * @return нормальная форма слова
+     */
+    private String getNormalWord(String word) {
+        var morphology = qualifyMorphology(word);
+        if (morphology == null) {
+            return "";
+        }
+
+        var normalForms = morphology.getNormalForms(word);
+
+        return normalForms.get(0);
+    }
+
+    /**
+     * Определяет морфологию по переданному слову, возвращает нужную библиотеку для использования
+     *
+     * @param word проверяемое слово
+     * @return библиотека морфологии, если морфология слова не поддерживается - null
+     */
+    private LuceneMorphology qualifyMorphology(String word) {
+        if (russianMorphology.checkString(word)) {
+            return russianMorphology;
+        } else if(englishMorphology.checkString(word)) {
+            return englishMorphology;
+        }
+
+        return null;
     }
 }
--- src/main/java/searchengine/services/SearchServiceImpl.java
+++ src/main/java/searchengine/services/SearchServiceImpl.java
@@ -115,7 +115,10 @@ public class SearchServiceImpl implements SearchService {
 
         var absoluteRelevance = computeAbsoluteRelevance(lemmas);
         var relativeRelevance = computeRelativeRelevance(absoluteRelevance);
-        var data = getSearchData(relativeRelevance, limit, offset);
+
+        var lemmasFinder = applicationContext.getBean(LemmasFinder.class);
+        var lemmasNames = lemmasFinder.findLemmas(query).keySet();
+        var data = getSearchData(relativeRelevance, limit, offset, lemmasNames);
 
         return SearchResponse.builder()
                 .result(true)
@@ -210,8 +213,9 @@ public class SearchServiceImpl implements SearchService {
                         entry -> entry.getValue() / maxRelevance));
     }
 
-    private List<SearchData> getSearchData(@NonNull Map<Page, Float> relevance, int limit, int offset) {
-        if (relevance.isEmpty() || offset > (relevance.size() - 1)) {
+    private List<SearchData> getSearchData(@NonNull Map<Page, Float> relevance, int limit, int offset,
+                                           @NonNull Set<String> lemmas) {
+        if (relevance.isEmpty() || offset > (relevance.size() - 1) || lemmas.isEmpty() || limit <= 0 || offset < 0) {
             return Collections.emptyList();
         }
 
@@ -220,6 +224,7 @@ public class SearchServiceImpl implements SearchService {
                 .toList();
 
         List<SearchData> data = new ArrayList<>(limit);
+        var lemmasFinder = applicationContext.getBean(LemmasFinder.class);
 
         var maxIndex = Math.min(offset + limit, relevance.size());
         for (int i = offset; i < maxIndex; i++) {
@@ -228,9 +233,11 @@ public class SearchServiceImpl implements SearchService {
             var site = page.getSite();
 
             var title = "";
+            var snippet = "";
             if (page.canBeParsed()) {
                 var document = Jsoup.parse(page.getContent());
                 title = document.title();
+                snippet = lemmasFinder.getSnippet(document.text(), lemmas);
             }
 
             var searchData = new SearchData();
@@ -238,7 +245,7 @@ public class SearchServiceImpl implements SearchService {
             searchData.setSiteName(site.getName());
             searchData.setUri(page.getPath());
             searchData.setTitle(title);
-            searchData.setSnippet("");
+            searchData.setSnippet(snippet);
             searchData.setRelevance(entry.getValue());
 
             data.add(searchData);
@@ -246,4 +253,5 @@ public class SearchServiceImpl implements SearchService {
 
         return data;
     }
+
 }
